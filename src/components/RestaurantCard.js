812a8f4971a0908e65628111f2475c923f94e465
Jyothika C S <jyothikachandrashekar@gmail.com>
2024-12-21 20:19:44 +0530

    CORS changes

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -9,7 +9,7 @@ const RestaurantCard = (props) => {
     cuisines,
     avgRating,
     costForTwo,
-    deliveryTime,
+    sla,
   } = resData.info;
   return (
     <div className="res-card" style={{ backgroundColor: "#f0f0f0" }}>
@@ -18,7 +18,7 @@ const RestaurantCard = (props) => {
       <h4>{cuisines.join(",")}</h4>
       <h4>{avgRating} stars</h4>
       <h4>{costForTwo}</h4>
-      <h4>{deliveryTime} minutes</h4>
+      <h4>{sla?.slaString}</h4>
     </div>
   );
 };
