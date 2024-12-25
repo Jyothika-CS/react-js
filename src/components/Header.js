16d9a6ea004522caf069f85846b43e705051dca7
Jyothika C S <jyothikachandrashekar@gmail.com>
2024-12-25 16:19:24 +0530

    Tailwind

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,30 +8,30 @@ const Header = () => {
 
   const onlineStatus = useOnlineStatus();
   return (
-    <div className="header">
+    <div className="flex justify-between shadow-lg my-4 sm:bg-yellow-50">
       <div className="logo-container">
-        <img className="logo" src={LOGO_URL} />
+        <img className="w-24" src={LOGO_URL} />
       </div>
-      <div className="nav-items">
-        <ul>
-          <li>
+      <div className="flex items-center">
+        <ul className="flex p-4 m-4">
+          <li className="px-4">
             Online Status: {onlineStatus ? "✅" : "🔴"}
           </li>
-          <li>
+          <li className="px-4">
             <Link to="/">Home</Link>
           </li>
-          <li>
+          <li className="px-4">
             <Link to="/about">About Us</Link>
           </li>
-          <li>
+          <li className="px-4">
             <Link to="/contact">Contact Us</Link>
           </li>
-          <li>
+          <li className="px-4">
             <Link to="/grocery">Grocery</Link>
           </li>
-          <li>Cart</li>
+          <li className="px-4">Cart</li>
           <button
-            className="login"
+            className="px-4 py-1 ml-1 bg-orange-500 rounded-lg"
             onClick={() => {
               btnName === "Login" ? setBtnName("Logout") : setBtnName("Login");
             }}
--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -3,20 +3,14 @@ import { CDN_URL } from "../utils/constants";
 const RestaurantCard = (props) => {
   const { resData } = props;
 
-  const {
-    cloudinaryImageId,
-    name,
-    cuisines,
-    avgRating,
-    costForTwo,
-    sla,
-  } = resData.info;
+  const { cloudinaryImageId, name, cuisines, avgRating, costForTwo, sla } =
+    resData.info;
   return (
-    <div className="res-card" style={{ backgroundColor: "#f0f0f0" }}>
-      <img alt="Food" className="res-logo" src={CDN_URL + cloudinaryImageId} />
-      <h3>{name}</h3>
+    <div className="m-4 p-4 w-[185px] rounded-lg bg-gray-100 hover:bg-gray-200 shadow-md" >
+      <img alt="Food" className="rounded-lg" src={CDN_URL + cloudinaryImageId} />
+      <h3 className="font-bold py-4 text-lg underline">{name}</h3>
       <h4>{cuisines.join(",")}</h4>
-      <h4>{avgRating} stars</h4>
+      <h4><span className="bg-green-500 rounded-md px-3">{avgRating}</span> stars</h4>
       <h4>{costForTwo}</h4>
       <h4>{sla?.slaString}</h4>
     </div>
